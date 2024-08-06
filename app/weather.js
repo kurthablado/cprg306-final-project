825a5577c30617118ae91bd571d42485cc2981d4
kurthablado <kurt.hablado@edu.sait.ca>
2024-08-06 10:13:31 -0600

    Update weather.js

--- app/weather.js
+++ app/weather.js
@@ -6,19 +6,24 @@ export default function Weather( {city, weatherObj}) {
     console.log("Received weatherObj", weatherObj);
 
     const {
-        temperature = "N/A",
-        wind = "N/A",
-        description = "no description available",
+        temperature: temperature,
+        wind: wind,
+        description: description,
     } = weatherObj || {};
 
         return(
             <div className="block ml-7 p-4 border-2 border-yellow-700 items-center text-center">
             <h2 className="text-xl font-bold pb-4">Weather in {city}:</h2>
-            <p><strong>Temperature:</strong> {temperature}</p>
-            <p><strong>Wind:</strong> {wind}</p>
-            <p><strong>Description:</strong> {description}</p>
-
-        </div>
+            {temperature && wind && description ? (
+                <>
+                    <p><strong>Temperature:</strong> {temperature}</p>
+                    <p><strong>Wind:</strong> {wind}</p>
+                    <p><strong>Description:</strong> {description}</p>
+                </>
+            ) : (
+                <p>No weather data available.</p>
+            )}
+            </div>
         );
     }
     
