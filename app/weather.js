a06702034fc800c32f5d54293149608f15551acc
kurthablado <kurt.hablado@edu.sait.ca>
2024-08-06 10:05:52 -0600

    styling

--- app/weather.js
+++ app/weather.js
@@ -12,8 +12,8 @@ export default function Weather( {city, weatherObj}) {
     } = weatherObj || {};
 
         return(
-            <div>
-            <h2 className="text-xl font-bold">Weather in {city}</h2>
+            <div className="block ml-7 p-4 border-2 items-center text-center">
+            <h2 className="text-xl font-bold pb-4">Weather in {city}:</h2>
             <p><strong>Temperature:</strong> {temperature}</p>
             <p><strong>Wind:</strong> {wind}</p>
             <p><strong>Description:</strong> {description}</p>
